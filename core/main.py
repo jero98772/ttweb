f2fe85525d98b9138677d7b52ad7399cbda92634
jero98772 <jero98772@protonmail.com>
2023-01-17 08:37:34 -0500

    update

--- core/main.py
+++ core/main.py
@@ -2,18 +2,45 @@
 # -*- coding: utf-8 -*- 
 #ttweb - by jero98772
 from flask import Flask, render_template, request, flash, redirect ,session
-from tools.dbInteracion import dbInteracion
-from tools.tools import *
+from core.tools.dbInteracion import dbInteracion
+from core.tools.tools import *
+app = Flask(__name__)
 app.secret_key = str(enPassowrdHash(generatePassword()))
 DBPATH = "data/"
+DATAUSER="data/datauser/"
 DBNAMEGAS = DBPATH + "db"
-app = Flask(__name__)
 class webpage():
-  @app.route("/")
+  @app.route("/",methods = ['GET','POST'])
   def index():
-    return render_template("index.html")
-  @app.route("/register")
+    data=""
+    if request.method == 'POST':
+      if session['status']:
+        if request.form["category"]:
+          session['status']=False
+          print("st1",session['status'])
+          data=request.form["category"]
+          writef(session['file'] ,formatin(data))
+      else:
+        print("st",session['status'],formatout())
+        writef(session['file'] ,formatout())
+        session['status']=True
+    return render_template("index.html",data=data)
+  @app.route("/login.html",methods = ['GET','POST'])
+  def login():
+    if request.method == 'POST':
+      db = dbInteracion(DBNAMEGAS)
+      db.connect("login")
+      usr = request.form["username"]
+      if db.findUser(usr) and db.findPassword(enPassowrdStrHex(request.form["password"])):
+        session['loged'] = True
+        session['user'] = usr
+        file=DATAUSER+usr+".ldg"
+        session['file'] = file
+        return redirect("/")
+    return render_template("login.html")
+  @app.route("/register",methods = ['GET','POST'])
   def register():
+    msg=""
     if request.method == 'POST':
       pwd = request.form["pwd"]
       pwd2 = request.form["pwd2"]
@@ -21,18 +48,34 @@ class webpage():
         usr = request.form["usr"]
         db = dbInteracion(DBNAMEGAS)
         db.connect("login")
-        if db.userAvailable(usr,"usr") :
-          encpwd = enPassowrdStrHex(pwd+usr) 
-          db.saveUser(usr,enPassowrdStrHex(pwd))
-          try:
-            createLedgerFile(encpwd)
-            session['loged'] = True
-            session['user'] = usr
-            session['file'] = encpwd
-          except db.userError():
-            return "invalid user , please try with other username and password"		
-    return render_template("register.html")
+        if db.findUser(usr):
+          msg="User alredy register"
+        else:
+          if db.userAvailable(usr,"usr") :
+            db.saveUser(usr,enPassowrdStrHex(pwd))
+            try:
+              file=DATAUSER+usr+".ldg"
+              createLedgerFile(file)
+              session['status']=True
+              session['loged'] = True
+              session['user'] = usr
+              session['file'] = file
+            except db.userError():
+              return "invalid user , please try with other username and password"		
+      else:
+        msg="Passwords must be same"
+    return render_template("register.html",msg=msg)
   @app.route("/unlog")
-  def register():
-  	session['loged']=False
+  def unlog():
+    session['loged']=False
     return redirect("/")
+  @app.route("/data.html")
+  def data():
+    data=""
+    try:
+      if session['loged']:
+        data=getbal(session['file'])
+        #print(session['file'])
+    except:
+        data="you need login"
+    return render_template("data.html",data=data)
--- core/tools/tools.py
+++ core/tools/tools.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- 
 #ttweb - by jero98772
+from hashlib import  sha256
+import base64
+import datetime
+from subprocess import run
 def enPassowrdStrHex(password):
 	password = str(password)
 	hashPassowrd = str(sha256(password.encode('utf-8')).hexdigest())
@@ -9,6 +13,23 @@ def enPassowrdHash(password):
 	password = str(password)
 	hashPassowrd = sha256(password.encode("utf-8")).digest()
 	return hashPassowrd
+def generatePassword():
+	"""generatePassword(),return srtring
+	generate random string 
+	"""
+	genPassowrd = ""
+	for i in range(0,16):
+		if len(genPassowrd) >= 16 and len(genPassowrd)-len(hexStr) <= 16:
+			num = rnd.randint(0,9999)
+			if 32 > num >126:
+				char = chr(num)
+				genPassowrd += char
+			else:
+				hexStr = str(hex(hexStr))
+				genPassowrd += hexStr
+		else:
+			break
+	return genPassowrd
 def createLedgerFile(name):
 	"""
 	writetxt(name,content) , write in txt file something  
@@ -16,3 +37,30 @@ def createLedgerFile(name):
 	with open(name, 'w') as file:
 		file.write("")
 		file.close()
+def readtxt(name):
+	"""
+	readtxt(name) , return txt content as array ,element by line 
+	"""
+	content = []
+	with open(name, 'r') as file:
+		for i in file.readlines():
+			content.append(str(i).replace("\n",""))
+	return reversed(content)
+def formatin(category):
+	now=datetime.datetime.today().strftime("%Y/%m/%d %H:%M:%S")
+	return "i "+now+" "+category+"\n"
+def formatout():
+	now=datetime.datetime.today().strftime("%Y/%m/%d %H:%M:%S")
+	return "o "+now+"\n"
+def writef(fname,content):
+
+	"""
+	writetxt(name,content) , write in txt file something  
+	"""
+	with open(fname, 'a') as file:
+		file.write(content)
+		file.close()
+#def outcategory():
+def getbal(fname,txt="data/tmp.txt"):
+	run(f"ledger -f {fname} bal > {txt}",shell=True)
+	return readtxt(txt)
